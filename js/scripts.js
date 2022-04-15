135235065c7767ae351487af64e1e5dc93b3ec8f
Amy Stamile <ak2224@nau.edu>
2022-04-15 16:33:37 -0700

    Added docs and fixed buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -102,10 +102,19 @@
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" color-red", "");
-    }
+    tablink = document.getElementById("pres1");
+    tablink.classList.remove("color-red");
+    tablink = document.getElementById("pres2");
+    tablink.classList.remove("color-red");
+    tablink = document.getElementById("pres3");
+    tablink.classList.remove("color-red");
+    tablink = document.getElementById("pres4");
+    tablink.classList.remove("color-red");
+    // for (i = 0; i < x.length; i++) {
+    //   console.log(tablinks[i].className);
+    //   tablinks[i].className = tablinks[i].className.replace(" color-red", " test");
+    //   console.log(tablinks[i].className);
+    // }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " color-red";
   }
